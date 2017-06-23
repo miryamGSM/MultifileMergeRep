97a9ed0879dd75eae2437f31166b80f783887bce
Violeta <vsanchezm@codicesoftware.com>
2017-06-23 10:54:09 +0000

    Delete foo. Move one method from foo to bar (modified difference).

--- bar.cs
+++ bar.cs
@@ -32,6 +32,37 @@ namespace GitMaster.LoginWindow
             mUserTextBox.Focus();
         }
 
+        Panel CreateContentErrorPanel(string message)
+        {
+            StackPanel result = new StackPanel();
+
+            TextBlock titleTextBlock = WebControlBuilder.CreateTitle(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.WaitingLicensePanelErrorTitle));
+            titleTextBlock.Margin = new Thickness(0, 40, 0, 15);
+
+            WebErrorPanel errorPanel = new WebErrorPanel();
+            errorPanel.ShowError(message);
+
+            mTeamInvitationCodeTextBox = WebControlBuilder.CreateTextBox(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.WaitingLicensePanelTeamInvitationCodeWatermark));
+
+            mGetLicenseButton = WebControlBuilder.CreateMainActionButton(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.GetLicenseButtonUppercase));
+            mGetLicenseButton.Click += GetLicenseButton_Click;
+
+            WebEntriesPacker.AddRelatedComponents(
+                result,
+                titleTextBlock,
+                errorPanel,
+                mTeamInvitationCodeTextBox,
+                mGetLicenseButton);
+
+            return result;
+        }
+
         internal void Dispose()
         {
             mSignUpLinkLabel.HyperLink.Click -= SignUpLinkLabel_Click;
