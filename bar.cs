857d493f89966b387e7d570d74139ac9d29adc27
Violeta <vsanchezm@codicesoftware.com>
2017-06-26 15:55:21 +0000

    Move one method from foo to a modified file, bar.

--- bar.cs
+++ bar.cs
@@ -57,6 +57,37 @@ namespace GitMaster.LoginWindow
             mLoginWindow.ShowSignUpPanel();
         }
 
+        Panel CreateContentErrorPanel(string message)
+        {
+            StackPanel result = new StackPanel();
+
+            TextBlock titleTextBlock = WebControlBuilder.CreateTitle(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.WaitingLicensePanelErrorTitle));
+            titleTextBlock.Margin = new Thickness(0, 40, 0, 15);
+
+            WebErrorPanel errorPanel = new WebErrorPanel();
+            errorPanel.ShowError(message);
+
+            mTeamInvitationCodeTextBox = WebControlBuilder.CreateTextBox(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.WaitingLicensePanelTeamInvitationCodeWatermark));
+
+            mGetLicenseButton = WebControlBuilder.CreateMainActionButton(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.GetLicenseButtonUppercase));
+            mGetLicenseButton.Click += GetLicenseButton_Click;
+
+            WebEntriesPacker.AddRelatedComponents(
+                result,
+                titleTextBlock,
+                errorPanel,
+                mTeamInvitationCodeTextBox,
+                mGetLicenseButton);
+
+            return result;
+        }
+
         void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             ClearErrors();
